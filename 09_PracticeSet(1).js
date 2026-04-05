570a7cdf3ad657ef1c5f21f69c29be7af73977d7
krukhaiya07-ux <krukhaiya07@gmail.com>
2026-04-05 14:53:39 +0530

    Initial Commit

---
+++ 09_PracticeSet(1).js
@@ -0,0 +1,8 @@
+//Practice Question:Create a variable of type string and try to add number to it
+
+let a ="Harry"//this variable contains a string values
+let b = 6
+console.log(a+b)
+
+//Use a Typeoff operator to find out datatype of string using a snippet of any code
+console.log(typeof (a+b))
