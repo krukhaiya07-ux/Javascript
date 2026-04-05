aedf7a83dcd1476a4200138b2f03416930ef4976
krukhaiya07-ux <krukhaiya07@gmail.com>
2026-04-05 12:59:19 +0530

    Create 08_TypesOfprimtiveDatatypes.js

---
+++ 08_TypesOfprimtiveDatatypes.js
@@ -0,0 +1,29 @@
+//Topic:Various Types of Basic Primitive Datatypes
+//Number Datatype:Used to hold Decimal values
+let z = 250;//
+    let y = 40.5;
+    console.log("Value of z=" + z);
+    console.log("Value of y=" + y);
+
+//String Datatype:Represents sequence of charachters
+let str = 'Hello All';
+let str1 = "Welcome to my new house";
+console.log("Value of str=" + str);
+console.log("Value of str1=" + str1);
+
+//Undefined:Value is explicitly set to undefined
+let x;
+console.log(x); // Outputs: undefined
+
+//Null datatype:serves as a placeholder
+let p = null;
+    console.log("Value of p=" + p);
+
+//BigInt Datatype:represents greater numbers
+let bigNum = 123422222222222222222222222222222222222n
+console.log(bigNum)
+
+//Symbol Datatype:used to create objects
+let sym = Symbol("Hello")
+console.log(typeof(sym));
+console.log(sym); 
