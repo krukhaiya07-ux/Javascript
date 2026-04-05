3e4b4f1a2cab271c88e7094de80697914b3f913c
krukhaiya07-ux <krukhaiya07@gmail.com>
2026-04-05 17:17:22 +0530

    Commit Changes

--- 10_PracticeSet(02).js
+++ 10_PracticeSet(02).js
@@ -1,2 +1,11 @@
+//Write a java script program to create a word meaning dictionary of 5 words.
 
+const dict={
+    appreciate:"Recognise full worth of.",
+    yakka :"hardwork.",
+    atraxia:"state of freedom from emotional disturbance.",
+    verbatim:"word-to-word;literaly.",
+    tenacious:"very determined;not giving up easily."
+}
 
+console.log(dict['yakka'])//or console.log(dict.yakka)
