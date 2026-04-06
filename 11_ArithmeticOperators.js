7de926cc3bd668ec3d688e53360ab4cd68f0ca67
krukhaiya07-ux <krukhaiya07@gmail.com>
2026-04-06 20:40:43 +0530

    Create 11_ArithmeticOperators.js

---
+++ 11_ArithmeticOperators.js
@@ -0,0 +1,17 @@
+//Topic:Types of Arithmetic Operators
+
+console.log("Operators in js")
+let a = 10;
+let b = 4;
+console.log("a+b=",a+b)
+console.log("a-b=",a-b)
+console.log("a/b=",a/b)
+console.log("a**b=",a**b)
+//INCREMENT USING OPERATORS
+console.log("++a=",++a)//Increments first, Store later;value=11
+console.log("a++=",a++)//increments in later stage, only store the data;value=12
+console.log("--a=",--a)//value=11
+console.log("a--=",a--)//value=10
+console.log("a=",a)// prints the current value 10
+console.log("a--=",a--)//value=9
+console.log("a=",a)
