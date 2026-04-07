06b13beef48e3ea69259c37f160701b52c7e799c
krukhaiya07-ux <krukhaiya07@gmail.com>
2026-04-07 19:12:18 +0530

    Initial Commit

--- 13_ConditionalExpressions.js
+++ 13_ConditionalExpressions.js
@@ -1,14 +1,31 @@
-let a=prompt("Whats your age?");
-a =Number.parseInt(a);
+//Topic:Conditional Expressions
+
+let a=5;
 if(a<0){
-    alert("This is an invalid age")
+    console.log("This is an invalid age")
 }
 else if(a<9){
-    alert("YOU ARE KID AND DONT THIK OF DRIVING!")  
+    console.log("YOU ARE KID AND DONT THIK OF DRIVING!")  
 }
 else if(a<18 && a>=9){
-    alert("YOU ARE STILL A KID BUT YPU MAY DRIVE AFTER 18")  
+    console.log("YOU ARE STILL A KID BUT YPU MAY DRIVE AFTER 18")  
 }
 else {
-    alert("you can now drive")  
+    console.log("you can now drive")  
+}
+
+//Switch Operator
+let day = 2;
+switch (day) {
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  default://If nothing matches
+    console.log("Invalid day");
 }
---
+++ 14_switchOperator.js
@@ -0,0 +1,20 @@
+//Topic:Switch operator for multiple values of one variable
+
+let grade = "A";
+
+switch (grade) {
+  case "A":
+    console.log("Excellent");
+    break;
+
+  case "B":
+    console.log("Good");
+    break;
+
+  case "C":
+    console.log("Average");
+    break;
+
+  default:
+    console.log("Fail");
+}
