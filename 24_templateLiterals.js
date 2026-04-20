4565975023fb2df43c96d95f27212d3e7d65fcea
krukhaiya07-ux <krukhaiya07@gmail.com>
2026-04-20 15:51:03 +0530

    Initial Commit

---
+++ 24_templateLiterals.js
@@ -0,0 +1,8 @@
+//Topic:Working of template literals in string 
+
+let num1=9
+let num2=3
+let result = num1 + num2
+
+console.log(`The addition of ${num1} and ${num2} is ${result}`);
+//console.log("The addition of" + num1 + "and" + num2 "is" + result)
